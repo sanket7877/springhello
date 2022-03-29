4f2c1ea205447761c305534bfaa90213432d8eba
sanket <lakhtriyasanket@gmail.com>
2022-03-29 18:05:20 +0530

    ff

--- src/main/java/com/example/hello/springhello/SpringhelloApplication.java
+++ src/main/java/com/example/hello/springhello/SpringhelloApplication.java
@@ -3,6 +3,7 @@ package com.example.hello.springhello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -13,7 +14,7 @@ public class SpringhelloApplication {
 		SpringApplication.run(SpringhelloApplication.class, args);
 	}
 
-	@GetMapping("/hello")
+	@RequestMapping("/")
 	public String helo(){
 		return "helloj";
 	}
