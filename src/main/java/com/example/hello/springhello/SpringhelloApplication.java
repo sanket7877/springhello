9cb5116814a143a4d93bb5be7978b0137ce52ab6
sanket <lakhtriyasanket@gmail.com>
2022-03-29 16:35:15 +0530

    Initial commit

---
+++ src/main/java/com/example/hello/springhello/SpringhelloApplication.java
@@ -0,0 +1,20 @@
+package com.example.hello.springhello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringhelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringhelloApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String helo(){
+		return "hello";
+	}
+}
