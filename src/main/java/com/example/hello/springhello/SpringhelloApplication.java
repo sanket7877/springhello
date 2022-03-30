36e182294a7b7737734c931d80d2136d95a9c5ba
sanket <lakhtriyasanket@gmail.com>
2022-03-30 11:39:43 +0530

    ff

--- src/main/java/com/example/hello/springhello/SpringhelloApplication.java
+++ src/main/java/com/example/hello/springhello/SpringhelloApplication.java
@@ -12,6 +12,7 @@ public class SpringhelloApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringhelloApplication.class, args);
+		System.out.println("added");
 	}
 
 	@RequestMapping("/")
